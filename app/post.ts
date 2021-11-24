3dc51fbb910edc381cb2c03e97940bcd0f8a569d
Danny Allegrezza <danny@dannyallegrezza.com>
2021-11-23 23:15:54 -0500

    swap out Markdown renderer

--- app/post.ts
+++ app/post.ts
@@ -2,12 +2,11 @@ import path from "path";
 import fs from "fs/promises";
 import parseFrontMatter from "front-matter";
 import invariant from "tiny-invariant";
-import { processMarkdown } from "@ryanflorence/md";
+import { marked } from "marked";
 
 export type Post = {
   slug: string;
   title: string;
-  body: string;
 };
 
 export type PostMarkdownAttributes = {
@@ -23,17 +22,17 @@ function isValidPostAttributes(
 }
 
 export async function getPost(slug: string) {
-  debugger;
   let filepath = path.join(postsPath, slug + ".md");
   let file = await fs.readFile(filepath);
   let { attributes, body } = parseFrontMatter(file.toString());
+
   invariant(
     isValidPostAttributes(attributes),
     `Post ${filepath} is missing attributes`
   );
-  console.log(body);
-  //let html = await processMarkdown(body);
-  return { slug, body, title: attributes.title };
+  let html = marked(body);
+
+  return { slug, html, title: attributes.title };
 }
 
 export async function getPosts() {
